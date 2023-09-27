31388156f9d1ec0c5db64148a9362b350a3538fc
Luc Comeau <luc.comeau@gmail.com>
2023-09-27 14:53:29 -0600

    Made a change for force an update to PyPi

--- SummitVentureStudioMarketAnalyzer/__init__.py
+++ SummitVentureStudioMarketAnalyzer/__init__.py
@@ -1,4 +1,4 @@
-from .ChatGPTApi import SvsChatGPTClass
 from .GovGrantsAPI import GovGrantsProcessor
 from .PatentAPI import PatentProcessor
 from .PitchBookAPI import PitchBookProcessor
+from .ChatGPTApi import SvsChatGPTClass
